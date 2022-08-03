ed211759a782cb7c8e6dce8c19ad4fb5cea19e58
HappyBajwa100 <happybajwa100@gmail.com>
2022-08-03 21:40:34 +1200

    saving changes

---
+++ reactProject-Learning-nad-Practice--master/src/components/Expenses/Expenses.js
@@ -0,0 +1,42 @@
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+import Card from "../UI/Card";
+import ExpensesFilter from "../ExpenseFilter/ExpenseFilter";
+import { useState } from "react";
+
+const Expenses = (props) => {
+  const [selectedYear, setSelectedYear] = useState("");
+  const filterChangeHandler = (dropDownYear) => {
+    setSelectedYear(dropDownYear);
+  };
+  
+  return (
+    <Card className="expenses">
+      <ExpensesFilter selected={selectedYear} onFilterChange={filterChangeHandler}></ExpensesFilter>
+      <ExpenseItem
+        title={props.items[0].title}
+        amount={props.items[0].amount}
+        date={props.items[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={props.items[1].title}
+        amount={props.items[1].amount}
+        date={props.items[1].date}
+      ></ExpenseItem>
+
+      <ExpenseItem
+        title={props.items[2].title}
+        amount={props.items[2].amount}
+        date={props.items[2].date}
+      ></ExpenseItem>
+
+      <ExpenseItem
+        title={props.items[3].title}
+        amount={props.items[3].amount}
+        date={props.items[3].date}
+      ></ExpenseItem>
+    </Card>
+  );
+};
+
+export default Expenses;
